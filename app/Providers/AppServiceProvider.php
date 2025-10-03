bb5c5a326e78ffdff07a5882bacf2f8fbd32b7d3
Mohamed Thioune <mokhouthioune96@gmail.com>
2025-10-03 14:20:21 +0000

    Normalize phone in routing, adjust config cors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,8 +29,20 @@ class AppServiceProvider extends ServiceProvider
         });
 
         Route::bind('phone', function ($value) {
-            return \App\Models\User::where('phone', $value)->firstOrFail();
+            // Normalize the numbers before searching
+            $normalized = $this->normalizePhone($value);
+
+            return \App\Models\User::where('phone', $normalized)->firstOrFail();
         });
 
+    }
+
+    //Normalize the phone number
+    public function normalizePhone(string $phone): string
+    {
+        // Save only the digits and "+" sign
+        $clean = preg_replace('/[^\d\+]/', '', $phone);
+
+        return $clean;
     }
 }
