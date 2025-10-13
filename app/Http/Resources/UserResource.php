b40e8de45f03c0b7a550a20cc6b8e875299c26db
Mohamed Thioune <mokhouthioune96@gmail.com>
2025-10-13 21:40:02 +0000

    User resource modified

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use App\Domain\Users\ValueObjects\Type;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
 
 class UserResource extends JsonResource
 {
@@ -17,19 +18,31 @@ class UserResource extends JsonResource
     {
         $role = ($this->roles) ? $this->roles->pluck('name')->toArray()[0] : Type::Customer->value;
         $this->load($role);
-        $childResource = match ($role) {
+        $childResources = match ($role) {
             Type::Customer->value   => CustomerResource::collection($this->whenLoaded('customer')),
             Type::Enterprise->value => EnterpriseResource::collection($this->whenLoaded('enterprise')),
             Type::Partner->value    => PartnerResource::collection($this->whenLoaded('partner')),
         };
+        $childResource = ($childResources) ? $childResources[0] : null;
+        if($role == Type::Customer->value):
+            $sigla = ($childResource->first_name) ? $childResource->first_name[0] . ' ' : null;
+            $sigla .= ($childResource->last_name) ? $childResource->last_name[0] : null;
+        else:
+            list($first, $last) = explode('.', $childResource->name, 2);
+            $sigla = isset($first) ? $first[0] . ' ' : null;
+            $sigla .= isset($last) ? $last[0] : null;
+        endif;
+
         return [
-            $role => ($childResource) ? $childResource[0] : null,
+            $role => $childResource,
             'id' => $this->id,
+            'sigla' => strtoupper($sigla),
             'email' => $this->email,
             'phone' => $this->phone,
             'whatsApp' => $this->whatsApp,
             'is_active' => $this->is_active,
-            'phone_verified_at' => $this->phone_verified_at
+            'phone_verified_at' => $this->phone_verified_at,
+            'user_registered_at' => $this->created_at->format('M Y'),
         ];
     }
 }
