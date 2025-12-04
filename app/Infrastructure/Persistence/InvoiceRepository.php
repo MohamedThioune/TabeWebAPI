e35745282e4918edf82acc72bcb41d70dadbf346
Mohamed Thioune <mokhouthioune96@gmail.com>
2025-12-04 04:05:30 +0000

    Refactor InvoiceAPIController and InvoiceRepository: clean up whitespace, update query logic to include gift card beneficiary search, and improve code readability in Invoice model.

--- app/Infrastructure/Persistence/InvoiceRepository.php
+++ app/Infrastructure/Persistence/InvoiceRepository.php
@@ -32,13 +32,12 @@ class InvoiceRepository extends BaseRepository
         $query = parent::allQuery($search, $skip, $limit);
         $q = $search['q'] ?? null;
 
-        // $query->whereHas('partner', function ($partner_query) use ($q) {
-        //     $partner_query->where('name', 'LIKE', '%' . trim($q) .'%');
-        // });
-        // $query->orWhereHas('user', function ($partner_query) use ($q) {
-        //     $partner_query->where('name', 'LIKE', '%' . trim($q) .'%');
-        // });
-        $query->where('amount', 'LIKE', '%' . trim($q) .'%');
+        $query->whereHas('gift_card', function ($gift_card_query) use ($q) {
+            $gift_card_query->whereHas('beneficiary', function ($beneficiary_query) use ($q) {
+                $beneficiary_query->where('full_name', 'LIKE', '%' . trim($q) .'%');
+            });
+        });
+        $query->orWhere('amount', 'LIKE', '%' . trim($q) .'%');
 
         return $query;
     }
