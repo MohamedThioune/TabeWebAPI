50a175bfbb266924fb905ea1ed1b664efa03c386
Mohamed Thioune <mokhouthioune96@gmail.com>
2025-12-22 21:12:05 +0000

    feat: Add GetTransactionAPIRequest and enhance Transaction model with validation rules; implement partner stats endpoint in UserAPIController

--- app/Http/Requests/API/GetTransactionAPIRequest.php
+++ app/Http/Requests/API/GetTransactionAPIRequest.php
@@ -3,26 +3,30 @@
 namespace App\Http\Requests\API;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Transaction;
 
 class GetTransactionAPIRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
-     */
+    */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
+    protected function prepareForValidation(): void
+    {
+        // Keep only the fields wanted
+        $this->replace($this->only(['status', 'filter_by_date', 'q', 'skip', 'limit', 'per_page', 'page']));
+    }
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
+    */
     public function rules(): array
     {
-        return [
-            //
-        ];
+        return Transaction::$rules_listed;
     }
 }
