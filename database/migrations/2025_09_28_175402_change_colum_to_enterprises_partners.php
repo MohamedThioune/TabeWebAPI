d906f980164f5e848d63ad78619750f98a0b398d
Mohamed Thioune <mokhouthioune96@gmail.com>
2026-05-19 22:15:47 +0000

    refactor: comment out enterprise table migration changes for UUID id

--- database/migrations/2025_09_28_175402_change_colum_to_enterprises_partners.php
+++ database/migrations/2025_09_28_175402_change_colum_to_enterprises_partners.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::table('partners', function (Blueprint $table) {
             $table->uuid('id')->change();
         });
-        Schema::table('enterprises', function (Blueprint $table) {
-            $table->uuid('id')->change();
-        });
+        // Schema::table('enterprises', function (Blueprint $table) {
+        //     $table->uuid('id')->change();
+        // });
     }
 
     /**
@@ -27,8 +27,8 @@ return new class extends Migration
         Schema::table('partners', function (Blueprint $table) {
             $table->integer('id')->change();
         });
-        Schema::table('enterprises', function (Blueprint $table) {
-            $table->integer('id')->change();
-        });
+        // Schema::table('enterprises', function (Blueprint $table) {
+        //     $table->integer('id')->change();
+        // });
     }
 };
